c310f49fcbacaa6609beec5601e056dc18c9c00b
James Etchberger <jamesetchberger@Jamess-MacBook-Air.local>
2022-07-28 15:48:14 -0400

    built out the firebase code and got some gets ready

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+const functions = require('firebase-functions')
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+//setup routes...
+app.get('/test', (req, res) => {
+  res.send('Wow, it actually worked.')
+})
+
+app.get('/anotherone', (req, res) => {
+  res.send('This one works too')
+})
+
+exports.api = functions.https.onRequest(app)
